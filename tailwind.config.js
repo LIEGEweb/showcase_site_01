868aaaacb76289d64b5a42ec82835081a07d35e0
Novitz Jean-Philippe <novitz@gmail.com>
2023-07-30 02:01:11 +0200

    new | asset | icons - svg files - bg in tailwind config

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,11 +1,26 @@
 /** @type {import('tailwindcss').Config} */
+// const colors = require('tailwindcss/colors')
+
 module.exports = {
     content: [
         "./assets/**/*.js",
         "./templates/**/*.html.twig",
     ],
     theme: {
-        extend: {},
+        extend: {
+            backgroundImage: {
+                'icon-phone': "url('../icons/phone.svg')",
+                'icon-envelope': "url('../icons/envelope.svg')",
+                'icon-map-pin': "url('../icons/map-pin.svg')",
+            },
+            colors: {
+                "primary" : "rgb(51 65 85)",
+                "primary-dark" : "rgb(148 163 184)",
+                "neutral" : "rgb(241 245 249)",
+                "neutral-dark" : "rgb(15 23 42)",
+
+            }
+        },
     },
     plugins: [],
 }
