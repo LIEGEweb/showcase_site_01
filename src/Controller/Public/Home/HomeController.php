1d3678af5f84dc5e316974c9c85f5ef98f4fa7e7
Novitz Jean-Philippe <novitz@gmail.com>
2023-08-02 00:16:55 +0200

    refactor (APP_HOME) - Home/Controller - home/index (twig)

--- src/Controller/Public/Home/HomeController.php
+++ src/Controller/Public/Home/HomeController.php
@@ -2,6 +2,9 @@
 
 namespace App\Controller\Public\Home;
 
+use App\Entity\CategoryGroup;
+use App\Repository\CategoryGroupRepository;
+use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
@@ -9,10 +12,10 @@ use Symfony\Component\Routing\Annotation\Route;
 class HomeController extends AbstractController
 {
     #[Route('/', name: 'app_home')]
-    public function index(): Response
+    public function index(EntityManagerInterface $entityManager): Response
     {
-        return $this->render('themes/'.$this->getParameter('app.theme').'/home/index.html.twig', [
-            'controller_name' => 'HomeController',
+        return $this->render('themes/' . $this->getParameter('app.theme') . '/home/index.html.twig', [
+            'servicesByCategoryGroup' => $entityManager->getRepository(CategoryGroup::class)->findAllWithServices(),
         ]);
     }
 }
