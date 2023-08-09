8bd9d2c71ec5e74e3bd796122a5c378cf6d94419
Novitz Jean-Philippe <novitz@gmail.com>
2023-08-09 03:32:08 +0200

    new (twig component) title/h2

---
+++ src/Components/Title/H3.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace App\Components\Title;
+
+use Symfony\UX\TwigComponent\Attribute\AsTwigComponent;
+
+#[AsTwigComponent (template: 'themes/%env(APP_THEME)%/components/title/H3.html.twig')]
+class H3
+{
+    public function __construct(public ?string $label = "",
+                                public ?bool $capitalize = false,
+                                public ?bool $uppercase = false,
+                                public ?string $id = ""
+    )
+    {
+    }
+}
