b762f706aff862e49a6b8e330f95ab91e435de35
Novitz Jean-Philippe <novitz@gmail.com>
2023-08-16 00:57:06 +0200

    add (entity) album
    - album entity
    - album repository

---
+++ src/Repository/AlbumRepository.php
@@ -0,0 +1,51 @@
+<?php
+
+namespace App\Repository;
+
+use App\Entity\Album;
+use App\Entity\Image;
+use Doctrine\Bundle\DoctrineBundle\Repository\ServiceEntityRepository;
+use Doctrine\ORM\EntityManagerInterface;
+use Doctrine\Persistence\ManagerRegistry;
+
+/**
+ * @extends ServiceEntityRepository<Album>
+ *
+ * @method Album|null find($id, $lockMode = null, $lockVersion = null)
+ * @method Album|null findOneBy(array $criteria, array $orderBy = null)
+ * @method Album[]    findAll()
+ * @method Album[]    findBy(array $criteria, array $orderBy = null, $limit = null, $offset = null)
+ */
+class AlbumRepository extends ServiceEntityRepository
+{
+    public function __construct(ManagerRegistry $registry, EntityManagerInterface $entityManager)
+    {
+        parent::__construct($registry, Album::class);
+    }
+
+
+//    /**
+//     * @return PhotosAlbum[] Returns an array of PhotosAlbum objects
+//     */
+//    public function findByExampleField($value): array
+//    {
+//        return $this->createQueryBuilder('p')
+//            ->andWhere('p.exampleField = :val')
+//            ->setParameter('val', $value)
+//            ->orderBy('p.id', 'ASC')
+//            ->setMaxResults(10)
+//            ->getQuery()
+//            ->getResult()
+//        ;
+//    }
+
+//    public function findOneBySomeField($value): ?PhotosAlbum
+//    {
+//        return $this->createQueryBuilder('p')
+//            ->andWhere('p.exampleField = :val')
+//            ->setParameter('val', $value)
+//            ->getQuery()
+//            ->getOneOrNullResult()
+//        ;
+//    }
+}
