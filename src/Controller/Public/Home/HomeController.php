913d8dab4af654ab057b538132b976408e992acf
Novitz Jean-Philippe <novitz@gmail.com>
2023-08-18 00:21:58 +0200

    add (HomeController) album.
    new albums variable and pass it to template.
    AlbumRepository findAllWithFirstImage() function is used

--- src/Controller/Public/Home/HomeController.php
+++ src/Controller/Public/Home/HomeController.php
@@ -6,6 +6,7 @@ use App\Entity\CategoryGroup;
 use App\Entity\News;
 use App\Repository\CategoryGroupRepository;
 use App\Repository\NewsRepository;
+use App\Repository\AlbumRepository;
 use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
@@ -15,7 +16,8 @@ class HomeController extends AbstractController
 {
     #[Route('/', name: 'app_home')]
     public function index(CategoryGroupRepository $categoryGroupRepository,
-                          NewsRepository $newsRepository): Response
+                          NewsRepository          $newsRepository,
+                          AlbumRepository         $albumRepository): Response
     {
         $servicesByCategoryGroup = $categoryGroupRepository->findAllWithServices();
 
@@ -31,7 +33,8 @@ class HomeController extends AbstractController
         return $this->render('themes/' . $this->getParameter('app.theme') . '/home/index.html.twig', [
             'servicesByCategoryGroup' =>  $servicesByCategoryGroup,
             "servicesImages" => $s,
-            "news" => $news
+            "news" => $news,
+            'albums' => $albumRepository->findAllWithFirstImage()
         ]);
     }
 }
