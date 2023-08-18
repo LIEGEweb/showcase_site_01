fb3634c1b1f68b2adc61495aaedfdb6f7298e2b9
Novitz Jean-Philippe <novitz@gmail.com>
2023-08-19 00:16:41 +0200

    refactor (twig component) Link
    Remove variables :
    - underline
    - uppercase
    - capitalize
    
    Add class merge attributes

--- src/Components/Link/Link.php
+++ src/Components/Link/Link.php
@@ -8,11 +8,9 @@ use Symfony\UX\TwigComponent\Attribute\AsTwigComponent;
 class Link
 {
     public function __construct(public ?string $label = "",
-                                public ?bool   $capitalize = false,
-                                public ?bool   $uppercase = false,
-                                public ?bool   $underline = true,
                                 public ?string $route = "",
                                 public ?string $fragment = "",
+                                public ?string  $slug = "",
                                 public ?string $hrefLink = "",
                                 public ?string $id = "",
     )
