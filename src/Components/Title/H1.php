e8814b78c4423464a86e19650dd5688b4571ce33
Novitz Jean-Philippe <novitz@gmail.com>
2023-08-21 00:26:18 +0200

    refactor (twig component) H1
    - remove variable **capitalise**
    - add merge class

--- src/Components/Title/H1.php
+++ src/Components/Title/H1.php
@@ -7,7 +7,7 @@ use Symfony\UX\TwigComponent\Attribute\AsTwigComponent;
 #[AsTwigComponent (template: 'themes/%env(APP_THEME)%/components/title/H1.html.twig')]
 class H1
 {
-    public function __construct(public ?string $label = "", public ?bool $capitalize = false, public ?string $id = "")
+    public function __construct(public ?string $label = "", public ?string $id = "")
     {
     }
 }
