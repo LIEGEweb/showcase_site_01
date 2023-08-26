33faf62a7ca514fb28d80a13636c1760af3e59a9
Novitz Jean-Philippe <novitz@gmail.com>
2023-08-27 00:21:09 +0200

    fix: contact component | controller part
    replace returned array by object

--- src/Components/Contact.php
+++ src/Components/Contact.php
@@ -2,6 +2,7 @@
 
 namespace App\Components;
 
+use App\Entity\Company;
 use App\Repository\CompanyRepository;
 use Doctrine\ORM\EntityManagerInterface;
 use Symfony\UX\TwigComponent\Attribute\AsTwigComponent;
@@ -12,6 +13,6 @@ class Contact
     public Object $contactInfos;
     public function __construct(CompanyRepository $companyRepository)
     {
-        $this->contactInfos = !empty($companyRepository->findAll()) ? $companyRepository->findAll()[0] : new stdClass();
+        $this->contactInfos = !empty($companyRepository->findAll()) ? $companyRepository->findAll()[0] : new Company();
     }
 }
