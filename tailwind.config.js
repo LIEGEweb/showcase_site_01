ce1ddc785eb10b10516c5499000bf8482a99d9c6
Novitz Jean-Philippe <novitz@gmail.com>
2023-08-29 04:40:24 +0200

    refactor tailwind cinfig file
    Add transition properties for
    - width
    - height
    
    Add custorm property aspect Ration .74

--- tailwind.config.js
+++ tailwind.config.js
@@ -10,13 +10,25 @@ module.exports = {
     theme: {
         extend: {
             backgroundImage: {
+                'icon-nurse-white': "url('../icons/user-nurse-white.svg')",
                 'icon-phone': "url('../icons/phone.svg')",
                 'icon-envelope': "url('../icons/envelope.svg')",
                 'icon-map-pin': "url('../icons/map-pin.svg')",
                 'icon-check': "url('../icons/check.svg')",
                 'icon-minus-small': "url('../icons/minus-small.svg')",
+                'icon-bars-2': "url('../icons/bars-2.svg') fill-white",
+                'splash-left': "url('../svg/splash-left.svg')"
             },
             colors: {
+                // NEXABYTE
+            /*    current: 'currentColor',
+                white: "#F5F5F5",
+                black: "rgb(0 0 0)",
+                secondary: "rgb(111 0 245)",
+                darkWhite: "rgb(2 6 23)",
+                darkBlack: "rgb(248 250 252)",
+                darkSecondary: "#219ebc",*/
+                /*DEFAULT*/
                 current: 'currentColor',
                 white: "rgb(248 250 252)",
                 black: "rgb(2 6 23)",
@@ -34,6 +46,16 @@ module.exports = {
             fontFamily: {
                 h1: "NotoSans-Black",
                 h2: "NotoSans-Bold",
+                "poppins-regular": [ 'Poppins-regular', "sans-serif"],
+                "poppins-semibold": [ 'Poppins-semibold', "sans-serif"],
+                "poppins-medium": [ 'Poppins-regular', "sans-serif"],
+            },
+            transitionProperty: {
+                'height': 'height',
+                'width': 'width',
+            },
+            aspectRatio: {
+                '74': '.74',
             },
         },
     },
