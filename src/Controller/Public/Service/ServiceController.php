0629fe711637b2e4483128ac152401158acc5be0
Novitz Jean-Philippe <novitz@gmail.com>
2023-09-09 00:45:43 +0200

    Refactor (public controller)  Service/index
    use paginator with query

--- src/Controller/Public/Service/ServiceController.php
+++ src/Controller/Public/Service/ServiceController.php
@@ -4,7 +4,9 @@ namespace App\Controller\Public\Service;
 
 
 use App\Repository\ServiceRepository;
+use Knp\Component\Pager\PaginatorInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
@@ -12,9 +14,13 @@ class ServiceController extends AbstractController
 {
 
     #[Route('/services', name: 'app_services')]
-    public function index(ServiceRepository $serviceRepository): Response
+    public function index(ServiceRepository $serviceRepository, PaginatorInterface $paginator, Request $request): Response
     {
-        $services = $serviceRepository->findAll();
+        $services = $paginator->paginate(
+            $serviceRepository->paginateServices(),
+            $request->query->getInt('page', 1),
+            $this->getParameter('app.news_per_page'));
+//        $services = $serviceRepository->findAll();
 
         return $this->render('themes/' . $this->getParameter('app.theme') . '/service/index.html.twig', [
             'services' => $services,
