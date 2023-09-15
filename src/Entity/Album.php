fc4c863b5d06813bd37d1e1dd55e62bf974127b7
Novitz Jean-Philippe <novitz@gmail.com>
2023-09-16 00:14:10 +0200

    refactor album entity

--- src/Entity/Album.php
+++ src/Entity/Album.php
@@ -41,6 +41,11 @@ class Album
         $this->images = new ArrayCollection();
     }
 
+    public function __toString(): string
+    {
+        return $this->name;
+    }
+
     public function getId(): ?int
     {
         return $this->id;
