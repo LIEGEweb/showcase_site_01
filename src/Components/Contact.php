a2ea49012fcd052af265bcd97cd9ea9e16231a88
Novitz Jean-Philippe <novitz@gmail.com>
2023-10-25 00:38:02 +0200

    Refactor contact components
    Removed the call to the Company repository becausea alreadey my (new) global DTO app_company_infos

--- src/Components/Contact.php
+++ src/Components/Contact.php
@@ -2,17 +2,9 @@
 
 namespace App\Components;
 
-use App\Entity\Company;
-use App\Repository\CompanyRepository;
-use Doctrine\ORM\EntityManagerInterface;
 use Symfony\UX\TwigComponent\Attribute\AsTwigComponent;
 
 #[AsTwigComponent (template:'themes/%env(APP_THEME)%/components/Contact.html.twig')]
 class Contact
 {
-    public Object $contactInfos;
-    public function __construct(CompanyRepository $companyRepository)
-    {
-        $this->contactInfos = !empty($companyRepository->findAll()) ? $companyRepository->findAll()[0] : new Company();
-    }
 }
