0ef431230dc6c3eb09c9b9512b0eaa2432257434
Novitz Jean-Philippe <novitz@gmail.com>
2023-12-11 01:45:39 +0100

    Update Contact Controller ans index template

--- src/Controller/Public/Contact/ContactController.php
+++ src/Controller/Public/Contact/ContactController.php
@@ -4,24 +4,24 @@ namespace App\Controller\Public\Contact;
 
 use App\Entity\Message;
 use App\Form\MessageType;
-use App\Repository\CompanyRepository;
-use App\Repository\MessageRepository;
 use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Mailer\MailerInterface;
+use Symfony\Component\Mime\Email;
 use Symfony\Component\Routing\Annotation\Route;
 
 class ContactController extends AbstractController
 {
     #[Route('/contact', name: 'app_contact')]
-    public function index(CompanyRepository $companyRepository, EntityManagerInterface $entityManager, Request $request): Response
+    public function index(EntityManagerInterface $entityManager, MailerInterface $mailer, Request $request): Response
     {
 
         $form = $this->createForm(MessageType::class);
         $form->handleRequest($request);
 
-        if ($form->isSubmitted() && $form->isValid()){
+        if ($form->isSubmitted() && $form->isValid()) {
             $datas = $form->getData();
             $message = new Message();
             $message->setName($datas->getName());
@@ -30,12 +30,25 @@ class ContactController extends AbstractController
             $message->setMessage($datas->getMessage());
             $entityManager->persist($message);
             $entityManager->flush();
-                $this->addFlash('success', 'Votre message a été envoyé.');
+
+            $email = (new Email())
+                ->from($datas->getEmail())
+                ->to('you@example.com')
+                //->cc('cc@example.com')
+                //->bcc('bcc@example.com')
+                //->replyTo('fabien@example.com')
+                //->priority(Email::PRIORITY_HIGH)
+                ->subject('Nouveau message de ' . $datas->getName())
+                ->text($datas->getMessage())
+                ->html($datas->getMessage());
+
+            $mailer->send($email);
+
+            $this->addFlash('success', 'Votre message a été envoyé.');
             return $this->redirectToRoute('app_contact');
 
         }
         return $this->render('themes/' . $this->getParameter('app.theme') . '/contact/index.html.twig', [
-            'company' => $companyRepository->getFirst(),
             'form' => $form->createView()
         ]);
     }
