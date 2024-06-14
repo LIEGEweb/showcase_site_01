2e496e0cb2fa97c805e7c661e670c9034ed66470
Novitz Jean-Philippe <novitz@gmail.com>
2024-06-15 00:11:20 +0200

    Fix  Global company infos

--- src/Dto/GlobalCompanyInfos.php
+++ src/Dto/GlobalCompanyInfos.php
@@ -8,53 +8,66 @@ use App\Repository\CompanyRepository;
 class GlobalCompanyInfos
 {
     private $infos;
+    private $repository;
 
     public function __construct(CompanyRepository $repository)
     {
-        if (!empty($repository->findAll())) $this->infos = $repository->findAll()[0];
-        else $this->infos = new Company();
-//        $this->infos =  $repository->findAll()[0];
+        $this->repository = $repository;
+    }
+
+    private function initializeInfos(): void
+    {
+        if ($this->infos === null) {
+            $companies = $this->repository->findAll();
+            $this->infos = !empty($companies) ? $companies[0] : new Company();
+        }
     }
 
     public function getName(): ?string
     {
+        $this->initializeInfos();
         return $this->infos->getName();
     }
 
     public function getVat(): ?string
     {
+        $this->initializeInfos();
         return $this->infos->getVat();
     }
 
-
     public function getPhone(): ?string
     {
+        $this->initializeInfos();
         return $this->infos->getPhone();
     }
 
     public function getEmail(): ?string
     {
+        $this->initializeInfos();
         return $this->infos->getEmail();
     }
 
     public function getStreetName(): ?string
     {
+        $this->initializeInfos();
         return $this->infos->getStreetName();
     }
 
     public function getStreetNr(): ?string
     {
+        $this->initializeInfos();
         return $this->infos->getStreetNr();
     }
 
     public function getPostalCode(): ?string
     {
-        return $this->infos->getPostalCity();
+        $this->initializeInfos();
+        return $this->infos->getPostalCode();
     }
 
     public function getPostalCity(): ?string
     {
+        $this->initializeInfos();
         return $this->infos->getPostalCity();
     }
-
 }
