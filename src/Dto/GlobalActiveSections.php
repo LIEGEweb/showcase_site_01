53b52aab0b6189990c799e036a2d5f149381faee
Novitz Jean-Philippe <novitz@gmail.com>
2024-06-19 00:07:47 +0200

    Add (twig) global variable GlobalActiveSections
    This global variable provide the information if a section is active or not.

---
+++ src/Dto/GlobalActiveSections.php
@@ -0,0 +1,50 @@
+<?php
+
+namespace App\Dto;
+
+use App\Entity\Company;
+use App\Repository\CompanyRepository;
+use App\Repository\SectionManagerRepository;
+
+class GlobalActiveSections
+{
+
+    private array $sections = [];
+
+    public function __construct(private SectionManagerRepository $manager)
+    {
+    }
+
+    private function initializeInfos(string $field): bool
+    {
+        if (in_array($field, $this->sections))
+            return $this->sections[$field];
+
+        if ($section = $this->manager->findOneBy(['name' => $field])) {
+            $this->sections[$field] = $section->isActive();
+            return $section->isActive();
+        }
+
+        return false;
+    }
+
+    public function hero()
+    {
+
+        return $this->initializeInfos('hero');
+//        if (in_array('hero', $this->sections))
+//            return $this->sections['hero'];
+//
+//        if ($section = $this->manager->findOneBy(['name' => 'hero'])) {
+//            $this->sections['hero'] = $section->isActive();
+//            return $section->isActive();
+//        }
+//
+//        return null;
+    }
+
+    public function service()
+    {
+        return $this->initializeInfos('service');
+    }
+}
