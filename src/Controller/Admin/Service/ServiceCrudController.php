bc38d67d3eab0f97b167c6664f445dd415cd9db0
Novitz Jean-Philippe <novitz@gmail.com>
2024-06-19 00:13:17 +0200

    Refactor Service Controllers
    - Service crud controller check if the service section is active or not.  Active-> return fields, not active-> return no field (empty array)
    
    - Service Controller (public) check if service section is active, if not redirect to app_home

--- src/Controller/Admin/Service/ServiceCrudController.php
+++ src/Controller/Admin/Service/ServiceCrudController.php
@@ -2,7 +2,10 @@
 
 namespace App\Controller\Admin\Service;
 
+use App\Entity\SectionManager;
 use App\Entity\Service;
+use App\Repository\SectionManagerRepository;
+use Doctrine\ORM\EntityManagerInterface;
 use EasyCorp\Bundle\EasyAdminBundle\Controller\AbstractCrudController;
 use EasyCorp\Bundle\EasyAdminBundle\Field\AssociationField;
 use EasyCorp\Bundle\EasyAdminBundle\Field\ImageField;
@@ -11,6 +14,9 @@ use EasyCorp\Bundle\EasyAdminBundle\Field\TextField;
 
 class ServiceCrudController extends AbstractCrudController
 {
+    public function __construct(private SectionManagerRepository $manager)
+    {
+    }
     public static function getEntityFqcn(): string
     {
         return Service::class;
@@ -19,6 +25,7 @@ class ServiceCrudController extends AbstractCrudController
 
     public function configureFields(string $pageName): iterable
     {
+        if ($this->manager->findOneBy(['name' => 'service'])->isActive())
         return [
             TextField::new('name')->setColumns(16),
             TextEditorField::new('description')->setColumns(16)->setNumOfRows(15),
@@ -27,6 +34,9 @@ class ServiceCrudController extends AbstractCrudController
                 ->setBasePath('images/services/')
                 ->setUploadDir('public/images/services/')
         ];
+        else {
+            return [];
+        }
     }
 
 }
--- src/Controller/Public/Service/ServiceController.php
+++ src/Controller/Public/Service/ServiceController.php
@@ -3,6 +3,7 @@
 namespace App\Controller\Public\Service;
 
 
+use App\Dto\GlobalActiveSections;
 use App\Repository\ServiceRepository;
 use Knp\Component\Pager\PaginatorInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
@@ -14,15 +15,18 @@ class ServiceController extends AbstractController
 {
 
     #[Route('/services', name: 'app_services')]
-    public function index(ServiceRepository $serviceRepository, PaginatorInterface $paginator, Request $request): Response
+    public function index(ServiceRepository $serviceRepository, PaginatorInterface $paginator, Request $request, GlobalActiveSections $activeSections): Response
     {
+        if(!$activeSections->service())
+            return $this->redirectToRoute('app_home');
+
         $services = $paginator->paginate(
             $serviceRepository->paginateServices(),
             $request->query->getInt('page', 1),
             $this->getParameter('app.news_per_page'));
 //        $services = $serviceRepository->findAll();
 
-        return $this->render('themes/' . $this->getParameter('app.theme') . '/service/index.html.twig', [
+        return $this->render('/service/index.html.twig', [
             'services' => $services,
         ]);
     }
