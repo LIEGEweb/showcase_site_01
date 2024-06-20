1dfd9fd76569e20223536c5d765f095e2b788c04
Novitz Jean-Philippe <novitz@gmail.com>
2024-06-20 23:41:39 +0200

    Refactor menu controller (stimulus)
    remove connect function that remove nav elements depending on screen size

--- assets/controllers/menu_controller.js
+++ assets/controllers/menu_controller.js
@@ -12,16 +12,16 @@ import {Controller} from '@hotwired/stimulus';
 export default class extends Controller {
     static targets = ["nav", "mobile", "desktop"]
 
-    connect() {
-
-        if (window.innerWidth > 1023) {
-            let element = this.mobileTarget
-            element.remove()
-        } else {
-            let element = this.desktopTarget
-            element.remove()
-        }
-    }
+    // connect() {
+    //
+    //     if (window.innerWidth > 1023) {
+    //         let element = this.mobileTarget
+    //         element.remove()
+    //     } else {
+    //         let element = this.desktopTarget
+    //         element.remove()
+    //     }
+    // }
 
     toggle() {
        if (this.mobileTarget !== undefined){
